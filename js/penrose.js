115695221769f8898626a3143c79bae3ead732ca
Blake Courter <bcourter@gmail.com>
2013-05-16 00:53:58 -0400

    Getting ready for subdiv: randomizing shape

--- js/penrose.js
+++ js/penrose.js
@@ -11,10 +11,10 @@ function init() {
     camera = new THREE.PerspectiveCamera(5, window.innerWidth / window.innerHeight, 1, 1000);
     camera.position.z = 70;
 
-    controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.addEventListener( 'change', render );
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.addEventListener('change', render);
 
-    window.addEventListener( 'resize', onWindowResize, false );
+    window.addEventListener('resize', onWindowResize, false);
 
     var Settings = function () {
         this.isRotatingCheckbox = document.getElementById("isRotating");
@@ -22,27 +22,27 @@ function init() {
     };
 
     materials = [
-        new THREE.MeshLambertMaterial( {
+        new THREE.MeshLambertMaterial({
             color: 0x222222,
             side: THREE.DoubleSide,
             shading: THREE.FlatShading,
             transparent: true,
             opacity: 0.5
-        } ),
-        new THREE.MeshBasicMaterial( {
+        }),
+        new THREE.MeshBasicMaterial({
             color: 0xEEEEEE,
             shading: THREE.FlatShading,
             wireframe: true
-        } )
+        })
     ];
 
 
     var loader = new THREE.OBJLoader();
-    loader.addEventListener( 'load', function ( event ) {
+    loader.addEventListener('load', function (event) {
         bodyGeometry = event.content;
     });
 
-    loader.load( "resources/obj/jig.obj" );
+    loader.load("resources/obj/jig.obj");
 
     settings = new Settings();
 }
@@ -51,44 +51,15 @@ function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 function animate() {
-    requestAnimationFrame( animate, renderer.domElement );
+    requestAnimationFrame(animate, renderer.domElement);
 
     render();
     controls.update();
     //stats.update();
-}
-
-function recurse(depth, object) {
-
-    var obj = object.clone();
-    var obj2 = object.clone();
-
-    obj.applyMatrix(new THREE.Matrix4().identity().rotateY(-Math.PI * 11 / 12).rotateZ(-Math.PI / 12));
-    obj.scale = new THREE.Vector3(0.6, 0.7, 0.6);
-    obj.translateX(0.8);
-    obj.translateZ(-0.5);
-
-    obj.translateY(0.6);
-    object.add(obj);
-
-    obj2.applyMatrix(new THREE.Matrix4().identity().rotateY(-Math.PI * 11 / 12).rotateZ(Math.PI / 12));
-    obj2.scale = new THREE.Vector3(0.6, 0.7, 0.6);
-    obj2.translateX(0.8);
-    obj2.translateZ(-0.5);
-
-    obj2.translateY(-0.6);
-    object.add(obj2);
-
-
-
-    if (depth == 0)
-        return;
-
-    recurse(depth - 1, object);
 }
 
 var lastTime = 0, lastAnimation = 0, lastRotation = 0;
@@ -103,28 +74,59 @@ function render() {
     lastTime = time;
 
     var scene = new THREE.Scene();
-    var triangle = new THREE.Object3D();
 
     if (bodyGeometry === undefined)
         return;
 
-    var geometry = bodyGeometry.clone();
-    triangle.add(geometry);
-    recurse(5, triangle);
+    var jig = bodyGeometry.clone();
 
-    triangle.applyMatrix(new THREE.Matrix4().identity()
-                         .rotateZ(Math.PI / 2)
-                         .rotateY(0.1 * lastRotation)
-                         );
+    jig.traverse(function (child) {
+        if (child instanceof THREE.Mesh) {
+            var faceIndices = ['a', 'b', 'c', 'd'];
+            var color, f, p, n, vertexIndex;
+            geometry = child.geometry;
 
-    var scale = 0.5;
-    triangle.scale.set(scale, scale, scale);
-    scene.add(triangle);
+            for (var i = 0; i < geometry.faces.length; i++) {
+                f = geometry.faces[i];
+                n = (f instanceof THREE.Face3) ? 3 : 4;
+
+                for (var j = 0; j < n; j++) {
+                    vertexIndex = f[faceIndices[j]];
+
+                    p = geometry.vertices[vertexIndex].clone();
+                    var speed = 4;
+                    var scale = 0.1;
+                    geometry.vertices[vertexIndex] = geometry.vertices[vertexIndex].add((new THREE.Vector3(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).multiplyScalar(scale)));
+
+                    color = new THREE.Color(0xffffff);
+
+
+                    var amplitude = 0.4;
+                    var gain = 0.6;
+                    p = p.addScalar((lastAnimation * speed) % Math.PI);
+
+                    var r = Math.sin(p.x) * Math.cos(p.y) * amplitude + gain;
+                    var g = Math.sin(p.y) * Math.cos(p.z) * amplitude + gain;
+                    var b = Math.sin(p.z) * Math.cos(p.x) * amplitude + gain;
+
+                    color.setRGB(r, g, b);
+                    f.vertexColors[j] = color;
+                }
+            }
+
+            geometry.colorsNeedUpdate = true;
+
+            child.material = new THREE.MeshBasicMaterial({ color: 0xffffff, vertexColors: THREE.VertexColors });
+        }
+
+    });
+
+    scene.add(jig);
 
     var ambientLight = new THREE.AmbientLight(0x666666);
     scene.add(ambientLight);
 
-    scene.fog = new THREE.Fog( 0x333333, 1500, 2100 );
+    scene.fog = new THREE.Fog(0x333333, 1500, 2100);
 
     var directionalLight = new THREE.DirectionalLight(0x8888aa);
     directionalLight.position.set(1, 1, 1).normalize();
@@ -134,5 +136,5 @@ function render() {
     directionalLight.position.set(-1, -1, -1).normalize();
     scene.add(directionalLight);
 
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
 }
